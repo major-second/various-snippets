3b06f4123920517d8c0cf5036c218e5ca37991f1
yozhang <major_seventh@outlook.com>
2023-03-19 15:49:37 +0800

    test of copying content

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const path = require('path');
+const fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -17,11 +19,24 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('various-snippets.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('various-snippets.copyContent', async function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Various Snippets!');
+		// Read the content of 'content.txt' in the extension's folder
+		const contentPath = path.join(context.extensionPath, 'content.txt');
+		const content = await fs.promises.readFile(contentPath, 'utf8');
+
+		// Get the currently opened folder
+		const workspaceFolders = vscode.workspace.workspaceFolders;
+		if (workspaceFolders && workspaceFolders.length > 0) {
+			const targetPath = path.join(workspaceFolders[0].uri.fsPath, 'tmp.hex');
+			await fs.promises.writeFile(targetPath, content);
+
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Content copied from content.txt to tmp.hex in the opened folder!');
+		} else {
+			vscode.window.showErrorMessage('No folder is currently opened.');
+		}
 	});
 
 	context.subscriptions.push(disposable);
